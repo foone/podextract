061136dbcc515b62a64a97453a147d70dc35bdc9
Foone Turing <fooneturing@gmail.com>
2020-09-05 23:43:49 -0700

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # podextract
  An extractor for POD archives from Terminal Reality games
+ 
+ Known games using this format are:
+
+ * Terminal Velocity (1995)
+ * Fury3 (1995)
+ * Hellbender (1996)
+ * Monster Truck Madness (1996)
+
+# Usage: 
+
+Simply run:
+
+``` python podextract.py PODNAME.pod "\*.MOD" ```
+
+(Using python 2.7)
+
+# File format details:
+There's a 16-bit file count at offset 0, then entries start at 0x54
+
+Each entry is 40 bytes long. The first 32 bytes are the filename(s), null-padded, and then the length and offset, as 32bit integers. 
+
+The filenames sometimes have two filenames packed into them: there'll be a first name, then a NUL byte, then another filename. I have no idea why this is.
---
+++ podextract.py
@@ -0,0 +1,41 @@
+import sys,struct,os,fnmatch
+files=[]
+if len(sys.argv)!=3:
+	print('Usage: {} <filepattern>'.format(sys.argv[0]))
+	print('Example: {} "*.MOD"'.format(sys.argv[0]))
+	sys.exit()
+else:
+	podfile=sys.argv[1]
+	pattern=sys.argv[2].upper()
+with open(podfile,'rb') as f:
+	f.seek(0)
+	num_files = struct.unpack('<L',f.read(4))[0]
+	f.seek(0x54)
+	for i in range(num_files):
+		parts=struct.unpack('<32sLL',f.read(0x28))
+		name,length,offset=parts
+		nameparts=name.split('\0',1)
+		if len(nameparts)==1:
+			main_name=nameparts[0].rstrip('\0')
+			sub_name=''
+		else:
+			main_name,sub_name=nameparts
+			sub_name=sub_name.rstrip('\0')
+		print(main_name, sub_name, length,offset)
+		files.append({
+			'name':main_name,
+			'sub_name':sub_name,
+			'length':length,
+			'offset':offset,
+		})
+
+	for file in files:
+		if fnmatch.fnmatch(file['name'].upper(),pattern):
+			f.seek(file['offset'])
+			data=f.read(file['length'])
+			name = file['name']
+			dirpath=os.path.dirname(name)
+			if not os.path.exists(dirpath):
+				os.makedirs(dirpath)
+			with open(name,'wb') as outf:
+				outf.write(data)
